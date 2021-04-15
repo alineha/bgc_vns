961796d0306f534f4578c62195ede93421a6703d
Aline <ahamorim@inf.ufrgs.br>
2021-04-14 21:27:49 -0300

    Comments

--- bgc.jl
+++ bgc.jl
@@ -5,21 +5,26 @@ struct Instance
     W          # Weight of each node
     function Instance(filepath)
         open(filepath) do file
+
+            ##### FIRST     LINE  #####
             line = split(readline(file)," ")
             v = parse(Int64,line[1])
             numEdges = parse(Int64,line[2])
             k = parse(Int64,line[3])
+
+            ##### SECOND    LINE  #####
             line = split(readline(file)," ")
             W = zeros(v)
             for i=1:v
                 W = parse(Float64,line[i])
             end
+
+            ##### REMAINING LINES #####
             lines = readlines(file)
             E = Array{Tuple{Int, Int}}(undef, first(size(lines)))
             for i=1:numEdges
                 line = split(lines[i], " ")
-                E[i]=(parse(Int64,line[1])+1,parse(Int64,line[2])+1) # Adds 1 to compensate the fact that Julia starts the arrays in 1 instead of 0
-                i = i+1
+                E[i]=(parse(Int64,line[1])+1,parse(Int64,line[2])+1) # Adds 1 to compensate the fact that Julia starts arrays in 1
             end
             new(v,E,k,W)
         end
@@ -33,7 +38,7 @@ struct Solution
     C
     m :: Int64
     function Solution(Instance)
-
+        
     end
 end
 
